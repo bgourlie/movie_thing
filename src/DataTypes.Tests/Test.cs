620e99a0285ec31d0b1e11e2f67efd8e786e697b
W. Brian Gourlie <bgourlie@gmail.com>
2015-04-21 00:29:41 -0500

    More work on heap impl.

---
+++ src/DataTypes.Tests/Test.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace DataTypes.Tests
+{
+	[TestFixture ()]
+	public class Test
+	{
+		[Test ()]
+		public void TestCase ()
+		{
+			var h = new Heap<int> ();
+			h.Add (3);
+			h.Add (2);
+			h.Add (1);
+			var blah = h.ToString ();
+			Console.WriteLine (blah);
+		}
+	}
+}
+
