f9f24d18ab162ffb6ba8f32408957c12d2897078
W. Brian Gourlie <bgourlie@gmail.com>
2015-04-21 21:48:00 -0500

    Implemented priority queue

---
+++ src/DataTypes.Tests/PriorityQueueTests.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace DataTypes.Tests
+{
+	[TestFixture]
+	public class PriorityQueueTests
+	{
+		[Test]
+		public void TestEnqueue ()
+		{
+			var h = new PriorityQueue<int> ();
+			h.Enqueue (312);
+			h.Enqueue (56);
+			h.Enqueue (1);
+			h.Enqueue (57);
+			h.Enqueue (3);
+			h.Enqueue (2);
+			h.Enqueue (14);
+			h.Enqueue (1);
+			h.Enqueue (6);
+			h.Enqueue (24);
+			h.Enqueue (789);
+			h.Enqueue (67);
+			h.Enqueue (99);
+			h.Enqueue (104);
+			h.Enqueue (103);
+			h.Enqueue (97);
+			CollectionAssert.AreEqual (new List<int> {1, 1, 3, 2, 6, 56, 57, 14, 312, 24, 789, 67, 99, 104, 103, 97}, h._arr);
+		}
+
+		[Test]
+		public void TestDequeue()
+		{
+			var h = new PriorityQueue<int> ();
+			h.Enqueue (10);
+			h.Enqueue (9);
+			h.Enqueue (8);
+			h.Enqueue (7);
+			h.Enqueue (6);
+			h.Enqueue (5);
+			h.Enqueue (4);
+			h.Enqueue (3);
+			h.Enqueue (2);
+			h.Enqueue (1);
+
+			Assert.AreEqual(1, h.Dequeue());
+			Assert.AreEqual(2, h.Dequeue());
+			Assert.AreEqual(3, h.Dequeue());
+			Assert.AreEqual(4, h.Dequeue());
+			Assert.AreEqual(5, h.Dequeue());
+			Assert.AreEqual(6, h.Dequeue());
+			Assert.AreEqual(7, h.Dequeue());
+			Assert.AreEqual(8, h.Dequeue());
+			Assert.AreEqual(9, h.Dequeue());
+			Assert.AreEqual(10, h.Dequeue());
+		}
+	}
+}
+
---
+++ src/DataTypes/PriorityQueue.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataTypes
+{
+	/// <summary>
+	/// A priority queue, implemented as a min heap.
+	/// </summary>
+	public class PriorityQueue<T> where T : IComparable<T>
+	{
+		internal readonly List<T> _arr = new List<T> ();
+		private int _ubound = -1;
+
+		public void Enqueue (T item)
+		{
+			if (_ubound == _arr.Count - 1) {
+				_arr.Add (item);
+			} else {
+				_arr [_ubound] = item;
+			}
+
+			_BalanceUp (++_ubound);
+		}
+
+		public T Dequeue()
+		{
+			var ret = _arr[0];
+			_arr[0] = _arr[_ubound--];
+			_BalanceDown(0);
+			return ret; 
+		}
+
+		public void _BalanceDown(int index)
+		{
+			int leftChild = index*2 + 1;
+			int rightChild = index*2 + 2;
+
+			if (leftChild > _ubound)
+			{
+				return;
+			}
+
+			// determine which child node is smaller
+			int indexToSwap = rightChild > _ubound 
+					? leftChild 
+					: _arr[leftChild].CompareTo(_arr[rightChild]) == -1 ? leftChild : rightChild;
+
+			// compare the current node with the smaller of the two child nodes
+			if (_arr[index].CompareTo(_arr[indexToSwap]) > 0)
+			{
+				// if the current node is bigger than the smaller of the two children,
+				// then continue to push that node down by swapping the two nodes				
+				var tmp = _arr[index];
+				_arr[index] = _arr[indexToSwap];
+				_arr[indexToSwap] = tmp;
+				_BalanceDown(indexToSwap);
+			}
+		}
+
+		private void _BalanceUp (int index)
+		{
+			if (index == 0) {
+				return;
+			}
+			int parentIdx = index / 2;
+
+			if (_arr [index].CompareTo (_arr [parentIdx]) < 0) {
+				var tmp = _arr [parentIdx];
+				_arr [parentIdx] = _arr [index];
+				_arr [index] = tmp;
+				_BalanceUp (parentIdx);
+			}
+		}
+	}
+}
+
