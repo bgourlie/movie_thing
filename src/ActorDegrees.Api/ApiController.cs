50ef9693938f9cb31ead0a4184d4751deb7a23d1
W. Brian Gourlie <bgourlie@gmail.com>
2015-05-18 22:30:40 -0500

    Add web project for REST api.

---
+++ src/ActorDegrees.Api/ApiController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace ActorDegrees.Api
+{
+    [Route("api/degrees")]
+    public class ApiController
+    {
+        [HttpGet]
+        public IActionResult GetDegrees()
+        {
+            return new ObjectResult(new {Thing="hello"});
+        }
+    }
+}
