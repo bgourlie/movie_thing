b11d232bf4acb4d55cb7eab8528d2f6a63feaa1e
W. Brian Gourlie <bgourlie@gmail.com>
2015-05-20 23:30:59 -0500

    PriorityQueue take 2.  Changes to accommodate Dijkstra's algorithm.

--- src/DataTypes.Tests/PriorityQueueTests.cs
+++ src/DataTypes.Tests/PriorityQueueTests.cs
@@ -9,51 +9,71 @@ namespace DataTypes.Tests
 		[Test]
 		public void TestEnqueue ()
 		{
-			var h = new PriorityQueue<int> ();
-			h.Enqueue (312);
-			h.Enqueue (56);
-			h.Enqueue (1);
-			h.Enqueue (57);
-			h.Enqueue (3);
-			h.Enqueue (2);
-			h.Enqueue (14);
-			h.Enqueue (1);
-			h.Enqueue (6);
-			h.Enqueue (24);
-			h.Enqueue (789);
-			h.Enqueue (67);
-			h.Enqueue (99);
-			h.Enqueue (104);
-			h.Enqueue (103);
-			h.Enqueue (97);
-			CollectionAssert.AreEqual (new List<int> {1, 1, 3, 2, 6, 56, 57, 14, 312, 24, 789, 67, 99, 104, 103, 97}, h._arr);
+			var h = new PriorityQueue<string> ();
+			h.Insert ("a", 312);
+			h.Insert ("b", 56);
+			h.Insert ("c", 1);
+			h.Insert ("d", 57);
+			h.Insert ("e", 3);
+			h.Insert ("f", 2);
+			h.Insert ("g", 14);
+			h.Insert ("h", 1);
+			h.Insert ("i", 6);
+			h.Insert ("j", 24);
+			h.Insert ("k", 789);
+			h.Insert ("l", 67);
+			h.Insert ("m", 99);
+			h.Insert ("n", 104);
+			h.Insert ("o", 103);
+			h.Insert ("p", 97);
+			CollectionAssert.AreEqual (new List<int> {1, 1, 3, 2, 6, 56, 57, 14, 312, 24, 789, 67, 99, 104, 103, 97}, h._keys);
 		}
 
 		[Test]
-		public void TestDequeue()
+		public void TestExtractMin()
 		{
-			var h = new PriorityQueue<int> ();
-			h.Enqueue (10);
-			h.Enqueue (9);
-			h.Enqueue (8);
-			h.Enqueue (7);
-			h.Enqueue (6);
-			h.Enqueue (5);
-			h.Enqueue (4);
-			h.Enqueue (3);
-			h.Enqueue (2);
-			h.Enqueue (1);
+			var h = new PriorityQueue<string> ();
+			h.Insert ("j", 10);
+			h.Insert ("i", 9);
+			h.Insert ("h", 8);
+			h.Insert ("g", 7);
+			h.Insert ("f", 6);
+			h.Insert ("e", 5);
+			h.Insert ("d", 4);
+			h.Insert ("c", 3);
+			h.Insert ("b", 2);
+			h.Insert ("a", 1);
 
-			Assert.AreEqual(1, h.Dequeue());
-			Assert.AreEqual(2, h.Dequeue());
-			Assert.AreEqual(3, h.Dequeue());
-			Assert.AreEqual(4, h.Dequeue());
-			Assert.AreEqual(5, h.Dequeue());
-			Assert.AreEqual(6, h.Dequeue());
-			Assert.AreEqual(7, h.Dequeue());
-			Assert.AreEqual(8, h.Dequeue());
-			Assert.AreEqual(9, h.Dequeue());
-			Assert.AreEqual(10, h.Dequeue());
+			Assert.AreEqual("a", h.ExtractMin());
+			Assert.AreEqual("b", h.ExtractMin());
+			Assert.AreEqual("c", h.ExtractMin());
+			Assert.AreEqual("d", h.ExtractMin());
+			Assert.AreEqual("e", h.ExtractMin());
+			Assert.AreEqual("f", h.ExtractMin());
+			Assert.AreEqual("g", h.ExtractMin());
+			Assert.AreEqual("h", h.ExtractMin());
+			Assert.AreEqual("i", h.ExtractMin());
+			Assert.AreEqual("j", h.ExtractMin());
+		}
+
+		[Test]
+		public void TestDecreaseKey()
+		{
+			var h = new PriorityQueue<string> ();
+			h.Insert ("j", 10);
+			h.Insert ("i", 9);
+			h.Insert ("h", 8);
+			h.Insert ("g", 7);
+			h.Insert ("f", 6);
+			h.Insert ("e", 5);
+			h.Insert ("d", 4);
+			h.Insert ("c", 3);
+			h.Insert ("b", 2);
+			h.Insert ("a", 1);
+            h.DecreaseKey("i", 0);
+
+			Assert.AreEqual("i", h.ExtractMin());
+			Assert.AreEqual("a", h.ExtractMin());
 		}
 	}
 }
--- src/DataTypes/PriorityQueue.cs
+++ src/DataTypes/PriorityQueue.cs
@@ -6,31 +6,68 @@ namespace DataTypes
 	/// <summary>
 	/// A priority queue, implemented as a min heap.
 	/// </summary>
-	public class PriorityQueue<T> where T : IComparable<T>
+	public class PriorityQueue<T>
 	{
-		internal readonly List<T> _arr = new List<T> ();
+		internal readonly List<T> _nodes = new List<T> ();
+	    internal readonly List<int> _keys = new List<int>();
+
+        // Using this as a way to look up a node's index.
+	    private readonly Dictionary<T, int> _indices = new Dictionary<T, int>();
+
 		private int _ubound = -1;
 
-		public void Enqueue (T item)
+		public void Insert (T item, int key)
 		{
-			if (_ubound == _arr.Count - 1) {
-				_arr.Add (item);
+		    int addedAt; 
+			if (_ubound == _nodes.Count - 1) {
+				_nodes.Add (item);
+                _keys.Add(key);
+			    addedAt = 0;
 			} else {
-				_arr [_ubound] = item;
+				_nodes [_ubound] = item;
+			    _keys[_ubound] = key;
+			    addedAt = _ubound;
 			}
 
+            _indices.Add(item, addedAt);
 			_BalanceUp (++_ubound);
 		}
 
-		public T Dequeue()
+		public T ExtractMin()
 		{
-			var ret = _arr[0];
-			_arr[0] = _arr[_ubound--];
+			var ret = _nodes[0];
+			_nodes[0] = _nodes[_ubound];
+		    _keys[0] = _keys[_ubound];
+		    _ubound -= 1;
 			_BalanceDown(0);
 			return ret; 
 		}
 
-		public void _BalanceDown(int index)
+	    public void DecreaseKey(T current, int newKey)
+	    {
+	        var index = _indices[current];
+	        var curKey = _keys[index];
+
+	        if (curKey == newKey)
+	        {
+	            return;
+	        }
+
+	        if (newKey > curKey)
+	        {
+	            throw new ArgumentException("newKey must have a lower value.");
+	        }
+
+	        _keys[index] = newKey;
+            _BalanceUp(index);
+	    }
+
+	    public bool Contains(T item)
+	    {
+	        return _indices.ContainsKey(item);
+	    }
+
+		private void _BalanceDown(int index)
 		{
 			int leftChild = index*2 + 1;
 			int rightChild = index*2 + 2;
@@ -43,16 +80,14 @@ namespace DataTypes
 			// determine which child node is smaller
 			int indexToSwap = rightChild > _ubound 
 					? leftChild 
-					: _arr[leftChild].CompareTo(_arr[rightChild]) == -1 ? leftChild : rightChild;
+					: _keys[leftChild] < _keys[rightChild] ? leftChild : rightChild;
 
 			// compare the current node with the smaller of the two child nodes
-			if (_arr[index].CompareTo(_arr[indexToSwap]) > 0)
+			if (_keys[index] > _keys[indexToSwap])
 			{
 				// if the current node is bigger than the smaller of the two children,
 				// then continue to push that node down by swapping the two nodes				
-				var tmp = _arr[index];
-				_arr[index] = _arr[indexToSwap];
-				_arr[indexToSwap] = tmp;
+                _SwapNodes(index, indexToSwap);
 				_BalanceDown(indexToSwap);
 			}
 		}
@@ -64,13 +99,25 @@ namespace DataTypes
 			}
 			int parentIdx = index / 2;
 
-			if (_arr [index].CompareTo (_arr [parentIdx]) < 0) {
-				var tmp = _arr [parentIdx];
-				_arr [parentIdx] = _arr [index];
-				_arr [index] = tmp;
+			if (_keys [index] < _keys [parentIdx]) {
+                _SwapNodes(index, parentIdx);
 				_BalanceUp (parentIdx);
 			}
 		}
+
+	    private void _SwapNodes(int idx1, int idx2)
+	    {
+            var tmpNode = _nodes [idx2];
+            _nodes [idx2] = _nodes [idx1];
+            _nodes [idx1] = tmpNode;
+
+	        var tmpKey = _keys[idx2];
+	        _keys[idx2] = _keys[idx1];
+	        _keys[idx1] = tmpKey;
+
+            _indices[_nodes[idx1]] = idx1;
+            _indices[_nodes[idx2]] = idx2;
+	    }
 	}
 }
 
