2851b16fcd1ab004dccd7f20bea6e90e2acaaaf6
W. Brian Gourlie <bgourlie@gmail.com>
2015-06-14 18:12:29 -0500

    Add WebAPI project

---
+++ src/ActorDegrees.Web/Controllers/DegreesController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Http;
+
+namespace ActorDegrees.Web.Controllers
+{
+    using System.Net;
+    using System.Net.Http;
+
+    [RoutePrefix("api/degrees")]
+    public class DegreesController : ApiController
+    {
+
+        public HttpResponseMessage Get(int actor1, int actor2)
+        {
+            var path = WebApiApplication.Graph.GetShortestPath(actor1, actor2);
+            return Request.CreateResponse(HttpStatusCode.OK, new { Result = "Hello"});
+        }
+    }
+}
---
+++ src/ActorDegrees.Web/Global.asax.cs
@@ -0,0 +1,23 @@
+﻿using System.Web;
+using System.Web.Http;
+
+namespace ActorDegrees.Web
+{
+    using System.IO;
+    using DataTypes;
+
+    public class WebApiApplication : HttpApplication
+    {
+        public static ReadonlyGraph Graph { get; private set; }
+
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            using (var stream = File.OpenRead(Server.MapPath("out.bin")))
+            {
+                Graph = ReadonlyGraph.NewFromStream(stream);
+            }
+        }
+    }
+}
