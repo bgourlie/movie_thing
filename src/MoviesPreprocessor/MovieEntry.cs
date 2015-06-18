e69598cf64752fc4645337c4c68b38522abfeba6
W. Brian Gourlie <bgourlie@gmail.com>
2015-06-17 21:28:06 -0500

    Prune movies based on cast being reachable in graph.

--- src/MoviesPreprocessor/MovieEntry.cs
+++ src/MoviesPreprocessor/MovieEntry.cs
@@ -15,7 +15,35 @@
 			Cast = cast;
 		}
 
-		public override string ToString()
+	    protected bool Equals(MovieEntry other)
+	    {
+	        return Id == other.Id;
+	    }
+
+	    public override bool Equals(object obj)
+	    {
+	        if (ReferenceEquals(null, obj)) return false;
+	        if (ReferenceEquals(this, obj)) return true;
+	        if (obj.GetType() != this.GetType()) return false;
+	        return Equals((MovieEntry) obj);
+	    }
+
+	    public override int GetHashCode()
+	    {
+	        return Id;
+	    }
+
+	    public static bool operator ==(MovieEntry left, MovieEntry right)
+	    {
+	        return Equals(left, right);
+	    }
+
+	    public static bool operator !=(MovieEntry left, MovieEntry right)
+	    {
+	        return !Equals(left, right);
+	    }
+
+	    public override string ToString()
 		{
 			return string.Format("{0} [{1}] starring {2}", Title, Year, string.Join(", ", Cast));
 		}
